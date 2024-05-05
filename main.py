d139c2e4d2e6fb0ef6ed315ca810039a7104876e
alesziegler <alesziegler@gmail.com>
2024-05-05 23:26:30 +0200

    Mapping solution space

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-print("Hello")
+
+
+class NamedTuple:
+    from collections import namedtuple
 
 class Produkt(NamedTuple):
-    pass
-    ...
+    """
+    ok, this class is clearly a child class of another class called
+    NamedTuple. It is not sufficient to import named tuple package, we probably
+    need to define that class.
+    Also, what is supposed to be the tuple? Whole 'produkty'?
+
+    """
+    def __init__(self,nazev,kategorie,cena,mnozstvi):
+        pass
+        #self.produkt = produkt
+        #self.produkt.namedtuple(produkt,['nazev','kategorie','cena','mnozstvi'])
 
 # Seznam produktů
 produkty = [
@@ -11,3 +23,4 @@ produkty = [
     Produkt(nazev="Kniha", kategorie="Knihy", cena=300.0, mnozstvi=10),
     Produkt(nazev="Tablet", kategorie="Elektronika", cena=8000.0, mnozstvi=0)
 ]
+
