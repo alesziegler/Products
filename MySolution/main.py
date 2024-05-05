97bfb8ea7988ec6c651438576c24de32025e3064
alesziegler <alesziegler@gmail.com>
2024-05-05 18:48:24 +0200

    Beginning

---
+++ MySolution/main.py
@@ -0,0 +1,11 @@
+class Produkt(NamedTuple):
+    pass
+    ...
+
+# Seznam produktů
+produkty = [
+    Produkt(nazev="Notebook", kategorie="Elektronika", cena=15000.0, mnozstvi=5),
+    Produkt(nazev="Mobilní telefon", kategorie="Elektronika", cena=8000.0, mnozstvi=10),
+    Produkt(nazev="Kniha", kategorie="Knihy", cena=300.0, mnozstvi=10),
+    Produkt(nazev="Tablet", kategorie="Elektronika", cena=8000.0, mnozstvi=0)
+]
