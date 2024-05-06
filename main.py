8edecdc6c60bf0a391878378265fc2490387c167
alesziegler <alesziegler@gmail.com>
2024-05-06 02:31:46 +0200

    Continuing exploration...

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 
-
+from collections import namedtuple
 class NamedTuple:
-    from collections import namedtuple
+    pass
 
 class Produkt(NamedTuple):
     """
     ok, this class is clearly a child class of another class called
     NamedTuple. It is not sufficient to import named tuple package, we probably
     need to define that class.
-    Also, what is supposed to be the tuple? Whole 'produkty'?
+    Also, what is supposed to be the tuple? Whole 'produkty'? Probably yes.
+    Every Produkt clearly function as something like a dictionary.
+    Dictionary is similar to named tuple, so Produkt is probably to be that.
+
 
     """
-    def __init__(self,nazev,kategorie,cena,mnozstvi):
-        pass
-        #self.produkt = produkt
-        #self.produkt.namedtuple(produkt,['nazev','kategorie','cena','mnozstvi'])
+    def __init__(self,nazev,
+                 kategorie,cena,mnozstvi
+                 ):
+        #self.nazev = nazev
+        #self.kategorie = kategorie
+        #self.cena = cena
+        #self.mnozstvi = mnozstvi
+        self.name = 'whatever'
+        self.produkt = namedtuple(self.name,['kategorie','cena','mnozstvi'
+                                              ])
+        # ok, it wants to use strings and integer as something like index names?
+        # so we maybe know how to create indexes, now, how to add values?
+        # maybe that is what two classes are for? Use super?
+        # it appears that name, here equivalent to "self.nazev", is used for a declaration?
+        # ok, now it sort of works, at least on first glance, except that
+        # we probably do need to ensure that nazev corresponds with name.
+        # so, translate to US format, and then use replace to replace bad characters?
 
 # Seznam produktů
 produkty = [
