41f1db03c5d65548c6578c00904e816498ff435c
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-05-14 14:07:39 +0000

    Trying to understand named tuples

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 
 from collections import namedtuple
+
+
 class NamedTuple:
     pass
+
 
 class Produkt(NamedTuple):
     """
     ok, this class is clearly a child class of another class called
     NamedTuple. It is not sufficient to import named tuple package, we probably
     need to define that class.
-    Also, what is supposed to be the tuple? Whole 'produkty'? Probably yes.
+    Also, what is supposed to be the tuple? Whole 'produkty'? Probably yes. Or not.
     Every Produkt clearly function as something like a dictionary.
-    Dictionary is similar to named tuple, so Produkt is probably to be that.
+    Dictionary is similar to named tuple, so Produkt is probably to be NamedTuple.
 
 
     """
@@ -21,9 +24,11 @@ class Produkt(NamedTuple):
         #self.kategorie = kategorie
         #self.cena = cena
         #self.mnozstvi = mnozstvi
-        self.name = 'whatever'
-        self.produkt = namedtuple(self.name,['kategorie','cena','mnozstvi'
+        #self.name = 'whatever'
+        self.produkt = namedtuple(nazev,[kategorie,cena,mnozstvi
                                               ])
+        # problem is that we need to abstractly declare named tuple in constuctor and concretely declare during initialization?
+# what is weird is why concrete declaration does have abstract values at all? But they are keys, they clearly have some function... But isn't an advantage of class setup that keys might be invisible?
         # ok, it wants to use strings and integer as something like index names?
         # so we maybe know how to create indexes, now, how to add values?
         # maybe that is what two classes are for? Use super?
@@ -40,3 +45,20 @@ produkty = [
     Produkt(nazev="Tablet", kategorie="Elektronika", cena=8000.0, mnozstvi=0)
 ]
 
+def dostupne_produkty(produkt):
+    """
+    This should return a list of NamedTuples? Parameter is also a list.
+    So list should be filtered and another list returned.
+    """
+    return produkt
+
+test_list = ["Kra", 52, "D"]
+
+result = filter(dostupne_produkty,produkty)
+
+for x in result:
+    print(x)
+
+print(produkty[1])
+
+Color = namedtuple('Color',['red'])
